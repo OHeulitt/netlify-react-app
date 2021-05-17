600a10a62392b8c21195103f812f88fa620ee5be
Olivia Heulitt <oheulitt120@gmail.com>
2021-05-17 11:34:35 -0700

    added corgis

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
         Carrot cake chocolate bar chocolate bear claw topping. Oat cake sesame snaps toffee chupa chups gingerbread pie. Jelly liquorice cake fruitcake cupcake. Tart fruitcake tiramisu lemon drops apple pie. Jelly beans gummi bears dragée powder. Cake marshmallow chocolate bar chocolate bar cake pastry. Chocolate bar pudding chocolate ice cream lollipop dragée cake pudding. Sesame snaps carrot cake lollipop dessert. Cotton candy fruitcake gummi bears powder marshmallow pudding dragée. Muffin caramels tart. Cake cupcake cupcake sweet. Liquorice tiramisu dessert jelly beans bonbon cake donut gummies tootsie roll. Cake gummi bears toffee icing bear claw ice cream chocolate cake.
 
-        Powder halvah danish oat cake soufflé gingerbread. Muffin apple pie toffee ice cream pudding bonbon carrot cake. Lemon drops soufflé sweet cake carrot cake. Lemon drops bear claw oat cake fruitcake powder bonbon pie. Donut tart donut. Biscuit pastry macaroon. Cake tootsie roll oat cake liquorice cotton candy chocolate cake. Marshmallow cupcake apple pie sweet roll lemon drops gingerbread cake cookie halvah. Sesame snaps brownie bear claw ice cream. Gummi bears donut ice cream sesame snaps. Gummies bonbon sweet. Topping chocolate bar macaroon. Cheesecake icing cookie.
+        <img src="http://placecorgi.com/200/200" alt="adorable corgi puppy" />
       </p>
     </div>
   );
